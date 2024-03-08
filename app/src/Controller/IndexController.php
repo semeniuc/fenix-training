779142adba35045e482c58be8496ae138930101a
Vadim Semeniuc <semeniuc@int.pl>
2024-03-08 12:37:57 +0100

    install app

--- app/src/Controller/IndexController.php
+++ app/src/Controller/IndexController.php
@@ -3,14 +3,21 @@
 namespace Beupsoft\Fenix\App\Controller;
 
 use Beupsoft\Fenix\App\Bitrix;
+use Symfony\Component\Dotenv\Dotenv;
 
 class IndexController 
 {
+    const C_REST_LOGS_DIR = "/logs";
     public function __construct()
     {
         // dd(["index"]);
+        $dotenv = new Dotenv();
+        $dotenv->load(dirname(__DIR__, 2) . '/.env');
 
-        echo Bitrix::call("scope");
+        $result = Bitrix::call("scope");
+
+        // $result = defined(static::class."::C_REST_LOGS_DIR");
+        echo json_encode($result);
 
         // echo "index";
     }
--- app/src/Controller/InstallController.php
+++ app/src/Controller/InstallController.php
@@ -1,11 +1,32 @@
-<?php 
+<?php
 
 namespace Beupsoft\Fenix\App\Controller;
 
-class InstallController 
+use Beupsoft\Fenix\App\Bitrix;
+
+class InstallController
 {
     public function __construct()
     {
-        dd(["install"]);
+        $this->response(Bitrix::installApp()['install']);
+    }
+
+    private function response(bool $isSuccessInstall): void
+    {
+        $htmlHead = 
+        "<head>
+            <script src=\"//api.bitrix24.com/api/v1/\"></script>
+            <script>
+                BX24.init(function () {
+                    BX24.installFinish();
+                });
+            </script>
+        </head>";
+
+        if ($isSuccessInstall === true) {
+            echo $htmlHead . "<body>installation has been finished</body>";
+        } else {
+            echo $htmlHead . "<body>installation error</body>";
+        }
     }
 }
