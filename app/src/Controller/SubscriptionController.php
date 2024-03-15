a70b32908bb5e1eba0f3187b0afac1b45ffcf3e3
Vadim Semeniuc <semeniuc@int.pl>
2024-03-15 06:57:44 +0100

    refactoring, save errors

--- app/src/Controller/SubscriptionController.php
+++ app/src/Controller/SubscriptionController.php
@@ -13,28 +13,35 @@ class SubscriptionController
     {
         while (!empty($events = $this->getEventsData())) {
             foreach ($events as $event) {
-                switch ($event["EVENT_NAME"]) {
-                    case "ONCRMDEALUPDATE":
-                        (new DealService())->handle($event["EVENT_DATA"]["FIELDS"]["ID"]);
-                        break;
-                    case "ONCRMDYNAMICITEMADD_149":
-                        (new TrainingService())->handle($event["EVENT_DATA"]["FIELDS"]["ID"]);
-                        break;
-                    case "ONCRMDYNAMICITEMUPDATE_149":
-                        (new TrainingService())->handle($event["EVENT_DATA"]["FIELDS"]["ID"]);
-                        break;
-                    case "ONCALENDARENTRYUPDATE":
-                        # code...
-                        break;
-                    case "ONCALENDARENTRYDELETE":
-                        # code...
-                        break;
-                    default:
-                        break;
-                }
+                    try {
+                        switch ($event["EVENT_NAME"]) {
+                            case "ONCRMDEALUPDATE":
+                                (new DealService())->handle($event["EVENT_DATA"]["FIELDS"]["ID"]);
+                                break;
+                            case "ONCRMDYNAMICITEMUPDATE_149":
+                            case "ONCRMDYNAMICITEMADD_149":
+                                (new TrainingService())->handle($event["EVENT_DATA"]["FIELDS"]["ID"]);
+                                break;
+                            case "ONCALENDARENTRYDELETE":
+                            case "ONCALENDARENTRYUPDATE":
+                                # code...
+                                break;
+                            default:
+                                break;
+                        }
+                    } catch (\Throwable $th) {
+                        Logging::save(
+                            [
+                                "code" => $th->getCode(),
+                                "message" => $th->getMessage(),
+                                "file" => $th->getFile(),
+                                "line" => $th->getLine()
+                            ],
+                            "throwable",
+                            "error"
+                        );
+                    }
             }
-
-            Logging::save($events, "events", "listener");
         }
     }
 
