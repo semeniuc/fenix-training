f540414fbf377d6d6262f6727c96ee826a68dbbe
Vadim Semeniuc <semeniuc@int.pl>
2024-03-23 19:09:45 +0100

    added upd check stage

--- app/src/Deal/Actions/CreateTrainingsAction.php
+++ app/src/Deal/Actions/CreateTrainingsAction.php
@@ -5,20 +5,17 @@ declare(strict_types=1);
 namespace Beupsoft\Fenix\App\Deal\Actions;
 
 use Beupsoft\Fenix\App\Deal\DealDTO;
-use Beupsoft\Fenix\App\Deal\Repository\DealRepository;
 use Beupsoft\Fenix\App\Deal\Repository\EventRepository;
 use Beupsoft\Fenix\App\Deal\Repository\TrainingRepository;
 use Beupsoft\Fenix\App\Deal\Tools\GenerateSchedule;
 
 class CreateTrainingsAction
 {
-    private DealRepository $dealRepository;
     private TrainingRepository $trainingRepository;
     private EventRepository $eventRepository;
 
     public function __construct(private readonly DealDTO $dealDTO)
     {
-        $this->dealRepository = new DealRepository();
         $this->trainingRepository = new TrainingRepository();
         $this->eventRepository = new EventRepository();
     }
@@ -75,7 +72,7 @@ class CreateTrainingsAction
         return $this->trainingRepository->createTrainings($data);
     }
 
-    private function getUnavailableTime(array $trainingsCollection)
+    private function getUnavailableTime(array $trainingsCollection): array
     {
         return $this->eventRepository->getUnavailableTime($trainingsCollection);
     }
