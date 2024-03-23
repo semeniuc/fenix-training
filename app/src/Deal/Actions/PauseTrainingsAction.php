53de09a8416226c1efc306dc5645c9e261e84cc8
Vadim Semeniuc <semeniuc@int.pl>
2024-03-23 19:15:50 +0100

    fix

--- app/src/Deal/Actions/PauseTrainingsAction.php
+++ app/src/Deal/Actions/PauseTrainingsAction.php
@@ -5,19 +5,16 @@ declare(strict_types=1);
 namespace Beupsoft\Fenix\App\Deal\Actions;
 
 use Beupsoft\Fenix\App\Deal\DealDTO;
-use Beupsoft\Fenix\App\Deal\Repository\DealRepository;
 use Beupsoft\Fenix\App\Deal\Repository\EventRepository;
 use Beupsoft\Fenix\App\Deal\Repository\TrainingRepository;
 
 class PauseTrainingsAction
 {
-    private DealRepository $dealRepository;
     private TrainingRepository $trainingRepository;
     private EventRepository $eventRepository;
 
     public function __construct(private readonly DealDTO $dealDTO)
     {
-        $this->dealRepository = new DealRepository();
         $this->trainingRepository = new TrainingRepository();
         $this->eventRepository = new EventRepository();
     }
